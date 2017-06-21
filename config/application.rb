39bdd3caf2ced7655458be64ef7eac7bd6f66dab
Joe Tessy <josephctessy@gmail.com>
2017-06-21 17:42:18 -0400

    Updateupdateupdate

--- config/application.rb
+++ config/application.rb
@@ -10,13 +10,14 @@ module BeatMachine
   class Application < Rails::Application
 
     config.paperclip_defaults = {
-          :storage => :s3,
-          :s3_region => ENV['AWS_REGION'],
-          :s3_credentials => {
-            :bucket => ENV['AWS_BUCKET'],
-            :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-            :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
-          }
+      :storage => :s3,
+      :s3_host_name => "s3-#{ENV["AWS_REGION"]}.amazonaws.com",
+      :s3_credentials => {
+        :bucket => ENV["s3_bucket"],
+        :access_key_id => ENV["s3_access_key_id"],
+        :secret_access_key => ENV["s3_secret_access_key"],
+        :s3_region => ENV["s3_region"],
+      }
     }
 
     # Settings in config/environments/* take precedence over those specified here.
