3dc7cbf9106095f5174a2b037534ba817dde119c
Joe Tessy <josephctessy@gmail.com>
2017-06-25 12:25:03 -0400

    Complete continuous play

--- frontend/components/player/player.jsx
+++ frontend/components/player/player.jsx
@@ -7,32 +7,32 @@ class Player extends React.Component {
   constructor(props){
     super(props);
     this.nextTrack = this.nextTrack.bind(this);
-    this.state = { sequenceCount: 0 };
+    this.state = { sequenceCount: 0, queue: [] };
   }
 
   componentWillReceiveProps(nextProps){
-    debugger;
+    this.setState({queue: this.props.queue});
     if (this.props.location.pathname !== nextProps.location.pathname){
       this.music.play();
-    } else if (this.props.queue.length === 0 &&
+    } else if (this.state.queue.length === 0 &&
     this.props.location.pathname === nextProps.location.pathname){
-      debugger;
       this.music.src = nextProps.queue[0].audio_url;
+      this.setState({queue: nextProps.queue})
       this.music.play();
       this.props.nowPlaying(nextProps.queue[0].id);
-    } else if (this.props.queue.length > 0 &&
-      nextProps.queue[0].audio_url === this.props.queue[0].audio_url){
-        debugger;
+    } else if (this.state.queue.length > 0 &&
+      nextProps.queue[0].audio_url === this.state.queue[0].audio_url){
       if (this.music.paused){
         this.music.play();
+        this.setState({queue: nextProps.queue});
         this.props.nowPlaying(nextProps.queue[0].id);
       } else {
         this.music.pause();
         this.props.nowPlaying(null);
       }
     } else {
-      debugger;
       this.music.pause();
+      this.setState({queue: nextProps.queue});
       this.music.src = nextProps.queue[0].audio_url;
       this.music.play();
       this.props.nowPlaying(nextProps.queue[0].id);
@@ -49,10 +49,11 @@ class Player extends React.Component {
 
   nextTrack(){
     let currentCount = this.state.sequenceCount;
-    this.setState({sequenceCount: currentCount + 1});
-    this.music.src =this.props.queue[this.state.sequenceCount].audio_url;
+    let newQueue = this.state.queue.slice(1);
+    this.setState({queue: newQueue});
+    this.music.src = this.state.queue[0].audio_url;
     this.music.play();
-    this.props.nowPlaying(this.props.queue[this.state.sequenceCount].id);
+    this.props.nowPlaying(this.state.queue[0].id);
   }
 
   render(){
