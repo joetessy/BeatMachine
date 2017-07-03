2fce7620cf0c8c8ba0061875c694b27f68e4fd32
Joe Tessy <josephctessy@gmail.com>
2017-07-03 18:00:37 -0400

    add https to avoid mixed content

--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,7 @@ module BeatMachine
       :storage => :s3,
       :s3_region => ENV["AWS_REGION"],
       :s3_credentials => {
+        :s3_protocol => 'https'
         :bucket => ENV["s3_bucket"],
         :access_key_id => ENV["s3_access_key_id"],
         :secret_access_key => ENV["s3_secret_access_key"],
