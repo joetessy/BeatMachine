bf960a935e540c3d20151a21b2ae24d7c390056a
Joe Tessy <josephctessy@gmail.com>
2017-07-03 18:05:06 -0400

    add https to avoid mixed content

--- config/application.rb
+++ config/application.rb
@@ -13,9 +13,9 @@ module BeatMachine
 
     config.paperclip_defaults = {
       :storage => :s3,
+      :s3_protocol => 'https',
       :s3_region => ENV["AWS_REGION"],
       :s3_credentials => {
-        :s3_protocol => 'https',
         :bucket => ENV["s3_bucket"],
         :access_key_id => ENV["s3_access_key_id"],
         :secret_access_key => ENV["s3_secret_access_key"],
