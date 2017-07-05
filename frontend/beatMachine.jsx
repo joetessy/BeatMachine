3122979338a1ae874b8d35819dbb8e15fb52e19c
Joe Tessy <josephctessy@gmail.com>
2017-07-05 15:09:41 -0400

    Removed some actions bound to the window

--- frontend/beatMachine.jsx
+++ frontend/beatMachine.jsx
@@ -2,11 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import configureStore from './store/store';
 import Root from './components/root';
-import { createFavorite } from "./actions/favorite_actions";
-
-
-window.createFavorite = createFavorite;
-
 
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('root');
@@ -18,6 +13,5 @@ document.addEventListener('DOMContentLoaded', () => {
   } else {
     store = configureStore();
   }
-  window.store = store;
   ReactDOM.render(<Root store={store}/>, root);
 });
